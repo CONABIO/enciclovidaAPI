a1be54307b43abeef6bcb7397e5772c0a0311ac8
Carlos Alonso <calonsogeek@gmail.com>
2022-09-23 12:15:27 -0500

    10 por pagina para que no se trabe la busqued por region

--- src/middlewares/openapi/schema/request/helper.request.js
+++ src/middlewares/openapi/schema/request/helper.request.js
@@ -2,7 +2,7 @@ const Joi = require("joi")
 
 const PaginadoReq = Joi.object().keys({
   pagina: Joi.number().min(1).max(1000000).default(1).required(),
-  por_pagina: Joi.number().valid(50, 100, 200).default(50).required(),
+  por_pagina: Joi.number().valid(10, 50, 100, 200).default(50).required(),
 })
 
 module.exports = PaginadoReq
